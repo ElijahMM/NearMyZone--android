275bbb0ecc2846ab1bf51cca2f0ce38d3c8b97ef
Mihai <ignatatassist@gmail.com>
2017-08-23 14:23:16 +0300

    added location

--- app/src/main/java/com/licenta/nearmyzone/Activities/MainActivity.java
+++ app/src/main/java/com/licenta/nearmyzone/Activities/MainActivity.java
@@ -1,8 +1,14 @@
 package com.licenta.nearmyzone.Activities;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.location.Location;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,10 +19,17 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.licenta.nearmyzone.Handlers.GPSLocation;
 import com.licenta.nearmyzone.R;
+import com.licenta.nearmyzone.Utils.Util;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -30,6 +43,8 @@ public class MainActivity extends AppCompatActivity
     @BindView(R.id.drawer_layout)
     DrawerLayout drawer;
 
+    private GPSLocation gpsLocation;
+    private GoogleMap gMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,8 +68,55 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-
+        gMap = googleMap;
+        if (!Util.askGpsPermission(MainActivity.this)) {
+            openDialogForLocation();
+        } else {
+            startLocationUpdate();
+        }
     }
+
+    public void startLocationUpdate() {
+        gpsLocation = new GPSLocation();
+        gpsLocation.getLocation(MainActivity.this, locationResult);
+    }
+
+    private GPSLocation.LocationResult locationResult = new GPSLocation.LocationResult() {
+        @Override
+        public void gotLocation(Location location) {
+            gMap.addMarker(new MarkerOptions()
+                    .position(new LatLng(location.getLatitude(),location.getLongitude()))
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
+
+            );
+            CameraUpdate center= CameraUpdateFactory.newLatLng(new LatLng(location.getLatitude(),location.getLongitude()));
+            CameraUpdate zoom=CameraUpdateFactory.zoomTo(15);
+            gMap.moveCamera(center);
+            gMap.animateCamera(zoom);
+        }
+    };
+
+    private void openDialogForLocation() {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+        dialog.setMessage("In order to get directions to Bizz Cafe you need to activate your phone location");
+        dialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent settingsIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivityForResult(settingsIntent, 111);
+            }
+        });
+
+        dialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        dialog.setCancelable(false);
+        dialog.show();
+    }
+
     @Override
     public void onBackPressed() {
         if (drawer.isDrawerOpen(GravityCompat.START)) {
@@ -74,10 +136,6 @@ public class MainActivity extends AppCompatActivity
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
@@ -88,5 +146,11 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
-
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 111) {
+            startLocationUpdate();
+        }
+    }
 }
---
+++ app/src/main/java/com/licenta/nearmyzone/Utils/AbsValues.java
@@ -0,0 +1,10 @@
+package com.licenta.nearmyzone.Utils;
+
+/**
+ * Created by Morgenstern on 08/23/2017.
+ */
+
+public class AbsValues {
+
+    public static final Integer GPS_REQUEST_INTERVAL =60000;
+}
